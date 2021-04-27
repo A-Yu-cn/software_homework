be41bb68816029d1d100cd369f09fd98f74c04e9
Ayu <2426774691@qq.com>
2021-04-27 23:05:17 +0800

    注释、信息完善

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import xml.etree.ElementTree as ET
 
 
 def create_code(model_name: str, template_name: str) -> None:
-    """test"""
+    """
+    此函数通过传入模型名称以及源代码模板路径，实现aquila模型转为JAVA源代码的功能
+    源代码将生成到项目路径下code文件夹下
+    """
     tree = ET.ElementTree(file=model_name)
     # 获取根节点
     root = tree.getroot()
     # 循环写好多个不同的类
     classnames = []
     for child_of_root in root:
-        # 这里进行每个类信息的构造
+        # 这里进行每个类所有信息的构造
         classnames.append(child_of_root.attrib['name'])
         s_abstract = ''
         classname = child_of_root.attrib['name']
@@ -24,14 +27,14 @@ def create_code(model_name: str, template_name: str) -> None:
         # 判断是否继承自其他类
         if (child_of_root.attrib.get('superTypes') != None):
             extendname = classnames[int(child_of_root.attrib.get('superTypes')[-1])]
-        # 获取该类的各种属性
+        # 获取该类的各种属性，属性类型在完善后可从默认String改为从xml文档中读出
         for elem in child_of_root.iterfind('structuralFeatures'):
             attr = {}
             attr['name'] = elem.attrib['name']
             attr['pb'] = 'private'
             attr['type'] = 'String'
             attrs.append(attr)
-        # 获取该类的各种操作
+        # 获取该类的各种操作，属性类型同上可改
         for elem in child_of_root.iterfind('operations'):
             func = {}
             func['name'] = elem.attrib['name']
@@ -42,21 +45,18 @@ def create_code(model_name: str, template_name: str) -> None:
         for elem in child_of_root.iterfind('dependencies'):
             if (elem.attrib['kind'] == 'call'):
                 is_call = '//call function is till not finished'
-
-        for elem in child_of_root.iterfind('dependencies'):
-            if (elem.attrib['kind'] == 'call'):
-                is_call = '//call function is till not finished'
         # 渲染模板
         env = Environment(loader=FileSystemLoader('.'))
         template = env.get_template(template_name)
         text = template.render(is_abstract=s_abstract, classname=classname, extendname=extendname, attrs=attrs,
                                funcs=funcs, is_call=is_call)
-        # 保存文件
+        # 保存到JAVA源代码文件中
         with open('./code/'+classname + '.java', 'w+') as f:
             f.write(text)
 
 
 if __name__ == '__main__':
+    #将两个路径都从方法中抽取出来，便于对不同模型和模板进行函数调用实现功能
     model_name = 'testt.aquila'
     template_name = 'test.txt'
     create_code(model_name, template_name)
